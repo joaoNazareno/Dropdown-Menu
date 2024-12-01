a87188a5ee9f64fdc6412d2534fa8aac69e84a4a
Joao <joaonaza24@icloud.com>
2024-11-30 22:57:50 -0300

    dropdown_menu

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ProfileMenu } from './components/ProfileMenu'; // Altere o caminho conforme necessário
+
+function App() {
+  return (
+    <div>
+      <ProfileMenu />
+    </div>
+  );
+}
+
+
+
+export default App
